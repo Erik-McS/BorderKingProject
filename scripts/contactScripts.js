8d16f9472ed4408df9a8d5c1a8c88c87eaf5f7d7
Erik McS <klaelfr60@outlook.com>
2023-01-16 09:15:48 +0000

    adding missing pages

---
+++ scripts/contactScripts.js
@@ -0,0 +1,25 @@
+
+// functions to validate the contact form
+
+function validateContactForm(){
+
+        //alert('validation event added');
+        globalEvent=event;
+        // creating the variable that will contains the text fields values.
+    
+        var firstname=document.getElementById('firstname');
+        var lastname=document.getElementById('lastname');
+        var email=document.getElementById('email');
+        var subject=document.getElementById('subject');
+        var comment=document.getElementById('comment');
+    
+        // create variables to manipulate the labels
+        var lb_firstname=document.getElementById('lb_firstname');
+        var lb_lastname=document.getElementById('lb_lastname');
+        var lb_email=document.getElementById('lb_email');
+        var lb_password=document.getElementById('lb_password');
+        var lb_subject=document.getElementById('lb_subject');
+        var lb_comment=document.getElementById('lb_comment');
+    
+
+}
